563da2349af4832ce20f3497ae30d3def331b019
Piyushshind <piyushshinde.199@gmail.com>
2025-08-06 09:33:13 +0530

    validate

--- src/index.js
+++ src/index.js
@@ -15,3 +15,24 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+const email1 = "apurva@example.com";
+const boolean = validateEmail(email1); // Output: true OR false
+
+if (boolean) {
+  console.log("Hi i am apurva");
+} else {
+  console.log(" I am not apurva");
+}
+
+const email2 = "invalid-email@com";
+const email3 = "example@.com";
+
+console.log(validateEmail(email2)); // Output: false
+console.log(validateEmail(email3)); // Output: false
